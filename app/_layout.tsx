d8b3a16e1dd2b78a86b326afe13d3c402c5ab5d6
Fash Muyhee <fashmuyidavid@gmail.com>
2024-09-02 23:52:00 +0100

    custom font setup  "SpaceGrotesk"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,19 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import React from "react";
 import { useEffect } from "react";
+import { Text, View } from "react-native";
 import "react-native-reanimated";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    regular: require("../assets/fonts/SpaceGrotesk-Regular.ttf"),
+    bold: require("../assets/fonts/SpaceGrotesk-Bold.ttf"),
+    light: require("../assets/fonts/SpaceGrotesk-Light.ttf"),
+    medium: require("../assets/fonts/SpaceGrotesk-Medium.ttf"),
   });
 
   useEffect(() => {
@@ -28,11 +27,8 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <View style={{ backgroundColor: "red", flex: 1, justifyContent: "center" }}>
+      <Text style={{ fontSize: 20, fontFamily: "medium" }}>Usage</Text>
+    </View>
   );
 }
