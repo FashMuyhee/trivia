4b70baa1d17d7d16399e417abf2e5f22bad6c98a
Fash Muyhee <fashmuyidavid@gmail.com>
2024-09-03 14:09:10 +0100

    feat add dmsans font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,23 @@
-import { theme } from "@/config";
-import { ThemeProvider } from "@shopify/restyle";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import React from "react";
-import { useEffect } from "react";
-import "react-native-reanimated";
+import {theme} from '@/config';
+import {ThemeProvider} from '@shopify/restyle';
+import {useFonts} from 'expo-font';
+import {Stack} from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import React from 'react';
+import {useEffect} from 'react';
+import 'react-native-reanimated';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    regular: require("../assets/fonts/SpaceGrotesk-Regular.ttf"),
-    bold: require("../assets/fonts/SpaceGrotesk-Bold.ttf"),
-    light: require("../assets/fonts/SpaceGrotesk-Light.ttf"),
-    medium: require("../assets/fonts/SpaceGrotesk-Medium.ttf"),
+  const [loaded, err] = useFonts({
+    regular: require('../assets/fonts/SpaceGrotesk-Regular.ttf'),
+    bold: require('../assets/fonts/SpaceGrotesk-Bold.ttf'),
+    light: require('../assets/fonts/SpaceGrotesk-Light.ttf'),
+    medium: require('../assets/fonts/SpaceGrotesk-Medium.ttf'),
+    dmsans: require('../assets/fonts/DMSans-ExtraBold.ttf'),
   });
+
 
   useEffect(() => {
     if (loaded) {
@@ -29,7 +31,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Stack initialRouteName="(tab)" screenOptions={{ headerShown: false }}>
+      <Stack initialRouteName="(tab)" screenOptions={{headerShown: false}}>
         <Stack.Screen name="(tab)" />
         <Stack.Screen name="(trivia)" />
       </Stack>
